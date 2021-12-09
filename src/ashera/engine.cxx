408f0b3382e6b6da810192f72f8681733c92a597
tbrekalo <brekalo.tvrtko@gmail.com>
2021-12-09 11:43:05 +0000

    changed the snp coverage approach

--- src/ashera/engine.cxx
+++ src/ashera/engine.cxx
@@ -26,14 +26,11 @@ namespace ashera {
 namespace detail {
 
 auto constexpr kMinimizeBatchCap = 1ULL << 32ULL;  // ~4GB
-auto constexpr kMapBatchCap = 1ULL << 30LLU;       // ~1GB
+auto constexpr kMapBatchCap = 1ULL << 30ULL;       // ~1GB
 
-auto constexpr kAlignBatchCap = 1ULL < 28ULL;  // ~250MB
+auto constexpr kPileBatchCap = 1ULL << 29ULL;  // ~512MB
 
 auto constexpr kFilterFrequency = 0.001;
-
-auto constexpr kMinAbsMisFreq = 3U;
-auto constexpr kMinRelMisFreq = 0.66;
 
 auto CreateMinimizerEngine(MinimapParams params) -> ram::MinimizerEngine {
   return ram::MinimizerEngine(GetThreadPoolPtr(), params.k, params.w,
@@ -41,47 +38,15 @@ auto CreateMinimizerEngine(MinimapParams params) -> ram::MinimizerEngine {
                               params.gap);
 }
 
-struct Mismatch {
-  std::uint32_t seq_id;
-  std::uint32_t pos;
-
-  std::uint32_t code;
+struct Coverage {
+  std::uint32_t a = 0U;
+  std::uint32_t t = 0U;
+  std::uint32_t c = 0U;
+  std::uint32_t g = 0U;
+  std::uint32_t i = 0U;
 };
 
-class MismatchCnt {
-  auto MaxElemeFreqSum() const noexcept {
-    auto const max_iter = std::max_element(cnts_.begin(), cnts_.end());
-    auto const freq_sum =
-        static_cast<float>(std::accumulate(cnts_.begin(), cnts_.end(), 0U));
-
-    return std::make_pair(max_iter, freq_sum);
-  }
-
- public:
-  MismatchCnt() : cnts_{0U, 0U, 0U, 0U} {}
-
-  auto Increment(std::uint8_t const code) noexcept -> void { ++cnts_[code]; }
-
-  auto CountFor(std::uint8_t const code) const noexcept -> std::uint32_t {
-    return cnts_[code];
-  }
-
-  auto MostFrequent() const noexcept -> std::pair<std::uint8_t, float> {
-    auto const [max_iter, freq_sum] = MaxElemeFreqSum();
-    auto const max_elem_code = std::distance(cnts_.begin(), max_iter);
-    // can avoid checking division by zero
-    // if the MismatchCnt is constructed; it at least has one entry
-    return std::make_pair(std::uint8_t(5), *max_iter / freq_sum);
-  };
-
-  auto DominantFreq() const noexcept -> std::pair<std::uint32_t, float> {
-    auto const [max_iter, freq_sum] = MaxElemeFreqSum();
-    return std::make_pair(*max_iter, *max_iter / freq_sum);
-  }
-
- private:
-  std::array<std::uint32_t, 4> cnts_;
-};
+using Pile = std::vector<Coverage>;
 
 }  // namespace detail
 
@@ -105,10 +70,14 @@ auto Engine::Correct(std::vector<std::unique_ptr<biosoup::NucleicAcid>>&& reads)
       reads.size(), std::vector<biosoup::Overlap>());
 
   auto overlaps_begin_idx = std::vector<std::uint32_t>(reads.size(), 0);
+  auto piles = std::vector<detail::Pile>();
 
-  auto mismatches =
-      std::vector<std::unordered_map<std::uint32_t, detail::MismatchCnt>>(
-          reads.size());
+  piles.reserve(reads.size());
+  std::transform(
+      reads.begin(), reads.end(), std::back_inserter(piles),
+      [](std::unique_ptr<biosoup::NucleicAcid> const& read) -> detail::Pile {
+        return detail::Pile(read->inflated_len);
+      });
 
   auto minimizer_engine = detail::CreateMinimizerEngine(MinimapParams());
   auto const thread_pool = GetThreadPoolPtr();
@@ -116,84 +85,92 @@ auto Engine::Correct(std::vector<std::unique_ptr<biosoup::NucleicAcid>>&& reads)
   timer.Start();
 
   {
-    auto const align_sequences =
-        [&](biosoup::Overlap const& ovlp) -> EdlibAlignResult {
-      auto const query = reads[ovlp.lhs_id]->InflateData(
+    auto const align_sequences = [&](biosoup::Overlap const& ovlp)
+        -> std::pair<std::string, EdlibAlignResult> {
+      auto const get_target = [&]() {
+        auto const inflated_data = reads[ovlp.rhs_id]->InflateData(
+            ovlp.rhs_begin, ovlp.rhs_end - ovlp.rhs_begin);
+        if (ovlp.strand) {
+          return inflated_data;
+        } else {
+          auto rc = biosoup::NucleicAcid("", inflated_data);
+          rc.ReverseAndComplement();
+
+          return rc.InflateData();
+        }
+      };
+
+      auto const query = reads[ovlp.rhs_id]->InflateData(
           ovlp.lhs_begin, ovlp.lhs_end - ovlp.lhs_begin);
 
-      auto const target = reads[ovlp.rhs_id]->InflateData(
-          ovlp.rhs_begin, ovlp.rhs_end - ovlp.rhs_begin);
+      auto const target = get_target();
 
-      return edlibAlign(
-          query.c_str(), query.size(), target.c_str(), target.size(),
-          edlibNewAlignConfig(-1, EDLIB_MODE_NW, EDLIB_TASK_PATH, nullptr, 0));
+      return std::make_pair(
+          std::move(query),
+          edlibAlign(query.c_str(), query.size(), target.c_str(), target.size(),
+                     edlibNewAlignConfig(-1, EDLIB_MODE_NW, EDLIB_TASK_PATH,
+                                         nullptr, 0)));
+    };
+
+    auto const update_pile = [&](std::uint32_t const pile_id) -> void {
+      for (auto const& ovlp : overlaps[pile_id]) {
+        auto const [target, edlibResult] = align_sequences(ovlp);
+
+        auto query_pos = ovlp.lhs_begin;
+        auto target_pos = 0U;
+
+        for (auto i = 0U; i < edlibResult.alignmentLength; ++i) {
+          switch (edlibResult.alignment[i]) {
+            case 0:
+            case 3: {
+              switch (target[target_pos]) {
+                /* clang-format off */
+                case 'A': ++piles[pile_id][query_pos].a; break;
+                case 'T': ++piles[pile_id][query_pos].t; break;
+                case 'C': ++piles[pile_id][query_pos].c; break;
+                case 'G': ++piles[pile_id][query_pos].t; break;
+                  /* clang-format on */
+              }
+
+              ++query_pos;
+              ++target_pos;
+
+              break;
+            }
+            case 1: {
+              ++piles[pile_id][query_pos].i;
+              ++query_pos;
+            }
+            case 2: {
+              ++target_pos;
+            }
+          }
+        }
+
+        edlibFreeAlignResult(edlibResult);
+      }
+    };
+
+    auto pile_futures = std::vector<std::future<void>>();
+    auto const update_piles = [&](std::size_t const batch_begin,
+                                  std::size_t const batch_end) -> void {
+      for (auto pile_id = batch_begin; pile_id < batch_end; ++pile_id) {
+        pile_futures.emplace_back(thread_pool->Submit(update_pile, pile_id));
+      }
     };
 
     // auto annotation_futures = std::vector<std::future<void>>();
-    auto annotation_futures =
-        std::vector<std::future<std::vector<detail::Mismatch>>>();
-
-    auto const submit_annotate_mismatches =
-        [&](biosoup::Overlap const& ovlp) -> std::vector<detail::Mismatch> {
-      auto dst = std::vector<detail::Mismatch>();
-
-      auto const edlib_result = align_sequences(ovlp);
-
-      auto const query_id = ovlp.lhs_id;
-      auto const target_id = ovlp.rhs_id;
-
-      auto query_pos = ovlp.lhs_begin;
-      auto target_pos = ovlp.rhs_begin;
-
-      for (auto i = 0U; i < edlib_result.alignmentLength; ++i) {
-        switch (edlib_result.alignment[i]) {
-          case 0:
-          case 3: {
-            dst.push_back(
-                detail::Mismatch{.seq_id = query_id,
-                                 .pos = query_pos,
-                                 .code = static_cast<std::uint8_t>(
-                                     reads[target_id]->Code(target_pos))});
-
-            dst.push_back(
-                detail::Mismatch{.seq_id = target_id,
-                                 .pos = target_pos,
-                                 .code = static_cast<std::uint8_t>(
-                                     reads[query_id]->Code(query_pos))});
-
-            ++query_pos;
-            ++target_pos;
-
-            break;
-          }
-          case 1: {
-            ++query_pos;
-            break;
-          }
-          case 2: {
-            ++target_pos;
-            break;
-          }
-          default: {
-            break;
-          }
-        }
-      }
-
-      return dst;
-    };
-
     auto const store_overlap = [&](biosoup::Overlap const& ovlp) -> void {
       overlaps[ovlp.lhs_id].push_back(ovlp);
       overlaps[ovlp.rhs_id].push_back(detail::ReverseOverlap(ovlp));
     };
 
     auto const find_batch_end_idx =
-        [&reads](std::size_t const begin_idx,
-                 std::size_t const end_idx) -> std::size_t {
+        [&reads](std::size_t const begin_idx, std::size_t const end_idx,
+                 std::uint64_t const kBackCapacity) -> std::size_t {
       auto sum = 0UL;
       auto curr_idx = begin_idx;
-      while (sum < detail::kMinimizeBatchCap && curr_idx < end_idx) {
+      while (sum < kBackCapacity && curr_idx < end_idx) {
         sum += reads[curr_idx++]->inflated_len;
       }
 
@@ -220,27 +197,16 @@ auto Engine::Correct(std::vector<std::unique_ptr<biosoup::NucleicAcid>>&& reads)
       for (auto& future : map_batch_futures) {
         for (auto&& ovlp : future.get()) {
           ovlp_sink(ovlp);
-          annotation_futures.emplace_back(
-              thread_pool->Submit(submit_annotate_mismatches, ovlp));
         }
       }
 
       map_batch_futures.clear();
-
-      // collect annotations
-      for (auto& future : annotation_futures) {
-        for (auto&& [seq_id, pos, code] : future.get()) {
-          mismatches[seq_id][pos].Increment(code);
-        }
-      }
-
-      annotation_futures.clear();
     };
 
     for (auto minimize_batch_begin = 0UL;
          minimize_batch_begin < reads.size();) {
-      auto const minimize_batch_end =
-          find_batch_end_idx(minimize_batch_begin, reads.size());
+      auto const minimize_batch_end = find_batch_end_idx(
+          minimize_batch_begin, reads.size(), detail::kMinimizeBatchCap);
 
       minimizer_engine.Minimize(std::next(reads.cbegin(), minimize_batch_begin),
                                 std::next(reads.cbegin(), minimize_batch_end),
@@ -253,11 +219,11 @@ auto Engine::Correct(std::vector<std::unique_ptr<biosoup::NucleicAcid>>&& reads)
               "[ashera::Engine::Correct]({:12.3f}) : minimized {} sequences\n"),
           timer.Stop(), (minimize_batch_end - minimize_batch_begin));
 
-      timer.Start();
       for (auto map_batch_begin = 0; map_batch_begin < minimize_batch_end;) {
-        auto const map_batch_end =
-            find_batch_end_idx(map_batch_begin, minimize_batch_end);
+        auto const map_batch_end = find_batch_end_idx(
+            map_batch_begin, minimize_batch_end, detail::kMapBatchCap);
 
+        timer.Start();
         map_batch(minimize_batch_begin, minimize_batch_end, store_overlap);
 
         fmt::print(
@@ -270,29 +236,31 @@ auto Engine::Correct(std::vector<std::unique_ptr<biosoup::NucleicAcid>>&& reads)
       }
       minimize_batch_begin = minimize_batch_end;
     }
+
+    for (auto pile_batch_begin = 0UL; pile_batch_begin < reads.size();) {
+      auto const pile_batch_end = find_batch_end_idx(
+          pile_batch_begin, reads.size(), detail::kPileBatchCap);
+
+      timer.Start();
+      update_piles(pile_batch_begin, pile_batch_end);
+
+      for (auto& it : pile_futures) {
+        it.wait();
+      }
+
+      pile_futures.clear();
+
+      fmt::print(stderr,
+                 FMT_COMPILE(
+                     "[ashera::Engine::Correct]({:12.3f}): updated {} piles\n"),
+                 timer.Stop(), (pile_batch_end - pile_batch_begin));
+
+      pile_batch_begin = pile_batch_end;
+    }
   }
 
   auto mstrm = std::ofstream("mismatches");
   auto log_vec = std::vector<std::uint32_t>();
-
-  for (auto read_id = 0U; read_id < reads.size(); ++read_id) { 
-    for (auto const [pos, misses] : mismatches[read_id]) {
-      auto const [abs_freq, rel_freq] = misses.DominantFreq();
-      if (abs_freq >= detail::kMinAbsMisFreq && rel_freq > detail::kMinRelMisFreq) {
-        log_vec.push_back(pos);  
-      }
-    }
-
-    if (!log_vec.empty()) {
-      mstrm << read_id << ' ';
-      for (auto const pos : log_vec) {
-        mstrm << pos << ' ';
-      }
-      mstrm << '\n';
-
-      log_vec.clear();
-    }
-  }
 
   std::flush(mstrm);
 
